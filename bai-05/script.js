2030f64bd780bbdddf746c01b65c75a54f18dc9d
levion8304 <ngocliem10a5nth@gmail.com>
2024-07-24 23:08:52 +0700

    BUỔI 05

---
+++ bai-05/script.js
@@ -0,0 +1,10 @@
+// var button = document.querySelector("button");
+// button.onclick = function () {
+//   alert("Hello world");
+// };
+
+var a = 10;
+var b = 30;
+var c = a + b;
+
+console.log(c);
