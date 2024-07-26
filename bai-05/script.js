d48f4ed849c30640ba1ffdfe7f403537480ae39c
levion8304 <ngocliem10a5nth@gmail.com>
2024-07-26 23:10:25 +0700

    Complete vid 6

--- bai-05/script.js
+++ bai-05/script.js
@@ -3,8 +3,44 @@
 //   alert("Hello world");
 // };
 
-var a = 10;
-var b = 30;
-var c = a + b;
+// String
+// var myString = `Xin chào! Tôi tên Nam. Tôi năm nay 18 tuổi.`;
+// console.log(myString.length)
+// console.log(myString.indexOf('Nam'))
+// var newString = myString.slice(3, myString.lastIndexOf(' '))
+// console.log(newString);
 
-console.log(c);
+// console.log(myString.replace('Tôi', 'Minh')) // Thay thế chuỗi tìm thấy đầu tiên
+// console.log(myString.replace(/Tôi/g, 'Minh')) // Thay thế tất cả chuỗi tìm thấy
+
+// console.log(myString.toUpperCase())
+
+// Number
+// var a = 10;
+// var b = "Test";
+// var result = a / b;
+// console.log(result)
+// console.log(typeof(result))
+// console.log(isNaN(result))
+// console.log(result.toString())
+
+// var a = 12.3456;
+// console.log(a.toFixed(1))
+
+// Array
+var list = ["HTML", "CSS3", "Javascript"]
+console.log(list.toString())
+console.log(list.join("-/-"))
+
+console.log(list.pop(), list)
+console.log(list.push('ReactJS', 'Bootstrap 4'), list)
+
+console.log(list.shift(), list)
+console.log(list.unshift("Bootstrap 4", "ReactJS"), list)
+
+console.log(list.splice(2, 0, "Bootstrap 4", "ReactJS"), list)
+console.log(list.splice(2, 1, "ReactJS"), list)
+
+console.log(list.concat(["ReactJS", "Bootstrap 4"]))
+
+console.log(list.slice(1, 8))
