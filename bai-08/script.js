41186dc00d2f946ded84543f6525dc834d20ff00
levion8304 <ngocliem10a5nth@gmail.com>
2024-08-02 11:27:28 +0700

    Vid 09 00:48:37

---
+++ bai-08/script.js
@@ -0,0 +1,37 @@
+// console.log(window);
+// console.log(window.innerHeight, window.innerWidth);
+
+// const infoUser = {
+//   name: "Le Van A",
+//   render: () => {
+//     console.log("OK");
+//   }
+// };
+// infoUser.render();
+
+// const openTab = () => {
+// 	window.open(
+// 		"https://28tech.com.vn/", 
+// 		"_blank", 
+// 		"width=1200, height=600, left=100, top=50"
+// 	);
+// }
+
+// let tab = null;
+// const openTab = () => {
+// 	tab = window.open(
+// 		"https://28tech.com.vn/", 
+// 		"_blank", 
+// 		"width=1200, height=600, left=100, top=50"
+// 	);
+// }
+
+// const closeTab = () => {
+// 	tab.close();
+// }
+
+// console.log(screen.width, screen.height);
+
+const reloadPage = () => {
+	location.reload();
+}
