3028ad06fe34bfb199f2f9161a24716ea6622419
levion8304 <ngocliem10a5nth@gmail.com>
2024-08-03 00:11:50 +0700

    Complete vid 9

--- bai-08/script.js
+++ bai-08/script.js
@@ -32,6 +32,53 @@
 
 // console.log(screen.width, screen.height);
 
-const reloadPage = () => {
-	location.reload();
+// const reloadPage = () => {
+// 	location.reload();
+// }
+
+// console.log(history);
+
+// console.log(navigator);
+
+// Tạo cookie
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + "; " + expires;
 }
+
+// const fullName = "Nam";
+// console.log(fullName);
+// setCookie("fullName", fullName, 3);
+
+// Lấy giá trị cookie
+var giatri = document.cookie
+console.log(giatri)
+var fullName = giatri.split("; ")[0].split("=")[1];
+console.log(fullName);
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+console.log(getCookie("phone"));
+//document.cookie = "phone=0123";
+console.log(getCookie("phone"));
+
+function deleteCookie(cname) {
+  document.cookie = `${cname}=;expires=Thu, 01 Jan 1970 00:00:00 UTC`;
+};
+
+
+
