0aa7efca984b15bfdee6b5725f9a2aff217f2fb6
levion8304 <ngocliem10a5nth@gmail.com>
2024-08-05 01:00:58 +0700

    Vid 10 01:13:30

---
+++ bai-09/script.js
@@ -0,0 +1,34 @@
+// var element = document.getElementById("h2-01");
+// element.innerText = "Heading H1";
+
+// const listH2 = document.getElementsByTagName("h2");
+// console.log(listH2);
+// for (const h2 of listH2) {
+//   console.log(h2);
+// }
+
+// const listTitle = document.getElementsByClassName("title");
+// console.log(listTitle);
+// for (const title of listTitle) {
+//   console.log(title);
+// }
+
+// const title = document.querySelector(".title");
+// console.log(title);
+
+// const listTagA = document.querySelectorAll("#menu li a");
+// console.log(listTagA);
+
+// const listTagA = document.getElementById("menu").getElementsByTagName("a");
+// console.log(listTagA);
+
+// const innerHTMLH2_01_text = document.querySelector("#h2-01").innerText;
+// const innerHTMLH2_01_html = document.querySelector("#h2-01").innerHTML;
+// console.log(innerHTMLH2_01_html, innerHTMLH2_01_text);
+
+// document.querySelector("#h2-01").innerText = "Test"
+
+const element = document.querySelector("#h2-01");
+const classText = element.getAttribute("class");
+console.log(classText);
+element.setAttribute("class", "text-2")
