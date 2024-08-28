d9b1c3e721be3c79e4aee9d3ef4524db6e074a13
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-08-28 22:44:36 +0700

    Bài 17 - 2. Tính năng phân trang (Tối ưu)

--- product-management/controllers/admin/product.controller.js
+++ product-management/controllers/admin/product.controller.js
@@ -2,18 +2,18 @@ const Product = require("../../model/products.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  
   const filterStatus = filterStatusHelper(req.query);
 
   let find = {
-    deleted: false
+    deleted: false,
   };
 
   if (req.query.status) {
-    find.status = req.query.status;  
+    find.status = req.query.status;
   }
 
   const objectSearch = searchHelper(req.query);
@@ -21,35 +21,28 @@ module.exports.index = async (req, res) => {
   if (objectSearch.regex) {
     find.title = objectSearch.regex;
   }
-  
+
   // Pagination
-  let objectPagination = {
-    limitItems: 4,
-    currentPage: 1
-  };
-
-  if (req.query.page) {
-    objectPagination.currentPage = parseInt(req.query.page);
-  }
-  objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
-
   const countProducts = await Product.countDocuments(find);
-  const totalPages = Math.ceil(countProducts / objectPagination.limitItems);
-
-  objectPagination.totalPages = totalPages;
+  let objectPagination = paginationHelper(
+    {
+      limitItems: 4,
+      currentPage: 1
+    },
+    req.query,
+    countProducts
+  );
   // End Pagination
 
-  const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
-  
+  const products = await Product.find(find)
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip);
+
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
     keyword: objectSearch.keyword,
-    pagination: objectPagination
-  })
+    pagination: objectPagination,
+  });
 };
-
-
-
-
