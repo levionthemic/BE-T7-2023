3afb64e6d5b6c17f78100223cea51b2d4a1c18dd
levi8304 <ngocliem10a5nth@gmail.com>
2024-09-11 16:41:43 +0700

    Config deploy online

--- product-management/index.js
+++ product-management/index.js
@@ -15,10 +15,10 @@ const session = require("express-session");
 const app = express();
 const port = process.env.PORT;
 
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 
 app.use(methodOverride("_method"));
 
