26d83dab507d6bdfbb6dcedd827906139b667570
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-09-13 13:16:36 +0700

    Fix lỗi số thứ tự

--- product-management/helpers/createTree.js
+++ product-management/helpers/createTree.js
@@ -1,8 +1,10 @@
+let count = 0;
 const createTree = (arr, parentId = "") => {
   const tree = [];
   arr.forEach((item) => {
     if (item.parent_id === parentId) {
       const newItem = item;
+      newItem.index = ++count;
       const children = createTree(arr, item.id);
       if (children.length) {
         newItem.children = children;
@@ -14,6 +16,7 @@ const createTree = (arr, parentId = "") => {
 }
 
 module.exports.tree = (arr, parentId = "") => {
+  count = 0;
   const tree = createTree(arr, parentId);
   return tree;
 };
