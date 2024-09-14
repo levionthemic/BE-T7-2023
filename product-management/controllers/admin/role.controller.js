8c94a6de0edc604c68cc432d1d39ab641a995071
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-09-14 13:31:37 +0700

    Bài 21 - 3.4. Xây dựng phần Chỉnh sửa nhóm quyền

--- product-management/controllers/admin/role.controller.js
+++ product-management/controllers/admin/role.controller.js
@@ -4,14 +4,14 @@ const systemConfig = require("../../config/system");
 // [GET] /admin/roles
 module.exports.index = async (req, res) => {
   let find = {
-    deleted: false
+    deleted: false,
   };
 
   const records = await Role.find(find);
 
   res.render("admin/pages/roles/index.pug", {
     pageTitle: "Nhóm quyền",
-    records: records
+    records: records,
   });
 };
 
@@ -20,14 +20,46 @@ module.exports.create = async (req, res) => {
   res.render("admin/pages/roles/create.pug", {
     pageTitle: "Tạo nhóm quyền",
   });
-}
+};
 
 // [POST] /admin/roles/create
 module.exports.createPost = async (req, res) => {
-
   const record = new Role(req.body);
   await record.save();
 
   req.flash("success", "Tạo mới thành công");
   res.redirect(`${systemConfig.prefixAdmin}/roles`);
-}
+};
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    let find = {
+      _id: id,
+      deleted: false,
+    };
+
+    const record = await Role.findOne(find);
+
+    res.render("admin/pages/roles/edit.pug", {
+      pageTitle: "Chỉnh sửa nhóm quyền",
+      record: record,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+  }
+};
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  try {
+    await Role.updateOne({ _id: req.params.id }, req.body);
+    req.flash("success", "Cập nhật thành công!");
+  } catch (error) {
+    req.flash("error", "Cập nhật thất bại!");
+  }
+
+  res.redirect("back");
+};
--- product-management/routes/admin/role.route.js
+++ product-management/routes/admin/role.route.js
@@ -8,4 +8,7 @@ routes.get("/", controller.index);
 routes.get("/create", controller.create);
 routes.post("/create", controller.createPost);
 
+routes.get("/edit/:id", controller.edit);
+routes.patch("/edit/:id", controller.editPatch);
+
 module.exports = routes;
