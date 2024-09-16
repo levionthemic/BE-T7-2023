8ad6769098422ec330f871cd61b3789f808ed03a
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-09-16 15:53:59 +0700

    Bài 23 - 1. Tạo authorization

--- product-management/middlewares/admin/auth.middleware.js
+++ product-management/middlewares/admin/auth.middleware.js
@@ -1,15 +1,18 @@
 const systemConfig = require("../../config/system");
 
 const Account = require("../../model/account.model");
+const Role = require("../../model/role.model");
 
 module.exports.requireAuth = async (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
   } else {
-    const user = await Account.findOne({ token: token });
+    const user = await Account.findOne({ token: token }).select("-password");
     if (user) {
-      console.log(token);
+      const role = await Role.findOne({ _id: user.role_id }).select("title permissions");
+      res.locals.user = user;
+      res.locals.role = role;
       next();
     } else {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
