10ee948fc95b9fddccdeea7ae200ed55a297b184
levionthemic <liem.levicoding834124@gmail.com>
2024-09-19 15:48:38 +0700

    Bài 26 - 1.3. Lấy data được gửi lên lưu vào database

--- product-management/controllers/client/checkout.controller.js
+++ product-management/controllers/client/checkout.controller.js
@@ -1,8 +1,9 @@
 const Cart = require("../../model/cart.model");
 const Product = require("../../model/products.model");
+const Order = require("../../model/order.model");
 const productHelper = require("../../helpers/product");
 
-// [GET] /
+// [GET] /checkout
 module.exports.index = async (req, res) => {
   const cartId = req.cookies.cartId;
   const cart = await Cart.findOne({ _id: cartId });
@@ -22,6 +23,51 @@ module.exports.index = async (req, res) => {
 
   res.render("client/pages/checkout/index.pug", {
     pageTitle: "Trang thanh toán",
-    cartDetail: cart
+    cartDetail: cart,
   });
+};
+
+// [POST] /checkout/order
+module.exports.order = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const userInfo = req.body;
+
+  const cart = await Cart.findOne({ _id: cartId });
+
+  let products = [];
+
+  for (const item of cart.products) {
+    const product = {
+      product_id: item.product_id,
+      price: 0,
+      discountPercentage: 0,
+      quantity: item.quantity,
+    };
+
+    const productInfo = await Product.findOne({ _id: item.product_id });
+    product.price = productInfo.price;
+    product.discountPercentage = productInfo.discountPercentage;
+
+    products.push(product);
+  }
+
+  const objectOrder = {
+    cart_id: cartId,
+    userInfo: userInfo,
+    products: products,
+  };
+
+  const order = new Order(objectOrder);
+  await order.save();
+
+  await Cart.updateOne(
+    {
+      _id: cartId,
+    },
+    {
+      products: []
+    }
+  )
+  req.flash("success", "Đặt hàng thành công!");
+  res.redirect(`/checkout/success/${order.id}`);
 };
--- product-management/routes/client/checkout.route.js
+++ product-management/routes/client/checkout.route.js
@@ -5,4 +5,6 @@ const routes = express.Router();
 
 routes.get("/", controller.index);
 
+routes.post("/order", controller.order);
+
 module.exports = routes;
