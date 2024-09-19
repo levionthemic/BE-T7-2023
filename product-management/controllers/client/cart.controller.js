a6bf1cf39361c02ee04b4ef335906e47fc179e6c
levionthemic <liem.levicoding834124@gmail.com>
2024-09-19 11:01:50 +0700

    Bài 25 - 2.3. Làm trang giỏ hàng

--- product-management/controllers/client/cart.controller.js
+++ product-management/controllers/client/cart.controller.js
@@ -1,4 +1,27 @@
 const Cart = require("../../model/cart.model");
+const Product = require("../../model/products.model");
+const productHelper = require("../../helpers/product");
+
+// [GET] /cart
+module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const cart = await Cart.findOne({ _id: cartId })
+
+  if (cart.products.length) {
+    for (const item of cart.products) {
+      const productId = item.product_id;
+      const product = await Product.findOne({ _id: productId });
+      product.priceNew = productHelper.priceNewProduct(product);
+      item.productInfo = product;
+    }
+    cart.totalPrice = cart.products.reduce((sum, item) => sum + item.quantity * item.productInfo.priceNew, 0);
+  }
+
+  res.render("client/pages/cart/index.pug", {
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart
+  }) 
+}
 
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
--- product-management/routes/client/cart.route.js
+++ product-management/routes/client/cart.route.js
@@ -3,6 +3,9 @@ const controller = require("../../controllers/client/cart.controller")
 
 const routes = express.Router();
 
+routes.get("/", controller.index);
+
 routes.post("/add/:productId", controller.addPost);
+
 
 module.exports = routes;
