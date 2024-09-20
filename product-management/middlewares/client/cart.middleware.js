8d2888b129b488f0be11e26827d7305de7f7e92d
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-09-20 10:16:01 +0700

    Fix bug

--- product-management/middlewares/client/cart.middleware.js
+++ product-management/middlewares/client/cart.middleware.js
@@ -14,11 +14,13 @@ module.exports.cartId = async (req, res, next) => {
     const cart = await Cart.findOne({
       _id: req.cookies.cartId,
     });
-    cart.totalQuantity = cart.products.reduce(
-      (sum, item) => sum + item.quantity,
-      0
-    );
-    res.locals.miniCart = cart;
+    if (cart) {
+      cart.totalQuantity = cart.products.reduce(
+        (sum, item) => sum + item.quantity,
+        0
+      );
+      res.locals.miniCart = cart;
+    }
   }
   next();
 };
