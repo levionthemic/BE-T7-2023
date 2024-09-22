c40304fe950f9abb3895476ba9200fc5e01b3167
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-09-22 12:13:02 +0700

    Bài 28 - 5.3. Hiển thị data real-time

--- product-management/controllers/client/chat.controller.js
+++ product-management/controllers/client/chat.controller.js
@@ -4,6 +4,7 @@ const User = require("../../model/user.model");
 // [GET] /chat
 module.exports.index = async (req, res) => {
   const user_id = res.locals.user.id;
+  const fullName = res.locals.user.fullName;
 
   // SocketIO
   // _io.on() sẽ connect mới khi load lại trang => database lưu nhiều record của 1 message
@@ -17,19 +18,26 @@ module.exports.index = async (req, res) => {
         content: content,
       });
       await chat.save();
+
+      // Trả data về client
+      _io.emit("SERVER_RETURN_MESSAGE", {
+        user_id: user_id,
+        fullName: fullName,
+        content: content
+      });
     });
   });
 
   const chats = await Chat.find({ deleted: false });
   for (const chat of chats) {
     const infoUser = await User.findOne({
-      _id: chat.user_id
+      _id: chat.user_id,
     }).select("fullName");
-    chat.infoUser = infoUser
+    chat.infoUser = infoUser;
   }
 
   res.render("client/pages/chat/index.pug", {
     pageTitle: "Chat",
-    chats: chats
+    chats: chats,
   });
 };
--- product-management/public/client/js/chat.js
+++ product-management/public/client/js/chat.js
@@ -5,10 +5,37 @@ if (formSendData) {
     e.preventDefault();
 
     const content = e.target.elements.content.value;
-    
+
     if (content) {
       socket.emit("CLIENT_SEND_MESSAGE", content);
       e.target.elements.content.value = "";
     }
-  })
+  });
 }
+
+// SERVER_RETURN_MESSAGE
+window.onload = () => {
+  const body = document.querySelector(".chat .inner-body");
+  body.scrollTop = body.scrollHeight;
+}
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+  
+  const body = document.querySelector(".chat .inner-body");
+  
+  const div = document.createElement("div");
+  const myId = document.querySelector("[my-id]").getAttribute("my-id");
+  if (myId != data.user_id) {
+    div.classList.add("inner-incoming");
+    div.innerHTML = `
+      <div class="inner-name">${data.fullName}</div>
+      <div class="inner-content">${data.content}</div>
+    `;
+  } else {
+    div.classList.add("inner-outgoing");
+    div.innerHTML = `
+      <div class="inner-content">${data.content}</div>
+    `;
+  }
+  body.appendChild(div);
+  body.scrollTop = body.scrollHeight;
+});
