4e7ca83bd4d9d64fc449e34bc776ecfe5cb1adc4
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-09-29 10:50:05 +0700

    Bài 34 - 2. Tìm kiếm

--- task-management/api/v1/controllers/task.controller.js
+++ task-management/api/v1/controllers/task.controller.js
@@ -1,6 +1,7 @@
 const Task = require("../models/task.model");
 
 const paginationHelper = require("../../../helpers/pagination");
+const searchHelper = require("../../../helpers/search");
 
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
@@ -23,6 +24,13 @@ module.exports.index = async (req, res) => {
   );
   // End Pagination
 
+  // Search
+  const objectSearch = searchHelper(req.query);
+
+  if (objectSearch.regex) {
+    find.title = objectSearch.regex;
+  }
+  // End Search
 
   // Sort 
   const sort = {};
