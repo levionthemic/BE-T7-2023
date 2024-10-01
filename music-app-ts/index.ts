c151ec67ece99aeda4667fbbe58c47e16b342559
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-10-01 17:46:54 +0700

    Bài 39 - 2.4. Kết nối với database

--- music-app-ts/index.ts
+++ music-app-ts/index.ts
@@ -1,7 +1,13 @@
 import express, { Express, Request, Response } from "express";
 
+import dotenv from "dotenv";
+dotenv.config();
+
+import * as database from "./config/database";
+database.connect();
+
 const app: Express = express();
-const port: number = 3000;
+const port: number | string = process.env.PORT || 3000;
 
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
