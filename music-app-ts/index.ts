68ad89281786d3e8241c5b0e6768b17325e6ab7b
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-10-01 18:27:51 +0700

    Bài 39 - 3.4. Nhúng file tĩnh

--- music-app-ts/index.ts
+++ music-app-ts/index.ts
@@ -11,6 +11,9 @@ import clientRoutes from "./routes/client/index.route";
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
+// Static
+app.use(express.static("public"));
+
 // Views PUG
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
