72bf581187c84204c129eb13999db98b646b8f45
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-10-01 18:00:42 +0700

    Bài 39 - 2.5. Lấy ra chủ đề bài hát

--- music-app-ts/index.ts
+++ music-app-ts/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import * as database from "./config/database";
+import Topic from "./models/topic.model";
 database.connect();
 
 const app: Express = express();
@@ -12,8 +13,11 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
-app.get("/topics", (req: Request, res: Response) => {
-  res.render("client/pages/topics/index.pug")
+app.get("/topics", async (req: Request, res: Response) => {
+  const topics = await Topic.find({ deleted: false });
+
+  console.log(topics);
+  res.render("client/pages/topics/index.pug");
 });
 
 app.listen(port, () => {
