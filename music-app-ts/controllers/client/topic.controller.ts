4c6e338a2421cd8406fbb67140ca0a1853c82243
levionthemic <liem.levicoding834124@gmail.com>
2024-10-01 20:06:29 +0700

    Bài 39 - 4. Làm trang Chủ đề bài hát (bên Client)

--- music-app-ts/controllers/client/topic.controller.ts
+++ music-app-ts/controllers/client/topic.controller.ts
@@ -5,9 +5,8 @@ import Topic from "../../models/topic.model";
 export const topics = async (req: Request, res: Response) => {
   const topics = await Topic.find({ deleted: false });
 
-  console.log(topics);
-
   res.render("client/pages/topics/index.pug", {
     pageTitle: "Chủ đề bài hát",
+    topics: topics
   });
 };
