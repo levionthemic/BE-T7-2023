94c0317c4dc2ef0ecd2a3cacab7c493faac5d0e9
levionthemic <liem.levicoding834124@gmail.com>
2024-10-02 21:47:36 +0700

    Bài 41 - 4. Tạo trang Danh sách bài hát (bên Admin)

---
+++ music-app-ts/routes/admin/song.route.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import * as controller from "../../controllers/admin/song.controller";
+
+const router: Router = Router();
+
+router.get("/", controller.index);
+
+export const songRoutes: Router = router;
